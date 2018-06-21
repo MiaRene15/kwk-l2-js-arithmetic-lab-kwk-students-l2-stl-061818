21a39b3f4a2e997560e977cf56a0401562626e56
MiaRene15 <kids.hines@icloud.com>
2018-06-21 14:14:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ let randomNumber = Math.random()
 
 let randomInteger = Math.floor(randomNumber)
 
-const randomUserID = Number.isInteger(randomUserID)
+let randomUserID = Number.isInteger(randomUserID)
